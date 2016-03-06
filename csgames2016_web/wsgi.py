7817d29373d47e1ec5ded07db24cd2ad5693a8c0
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-03-06 11:54:16 -0500

    Initial Django project

---
+++ csgames2016_web/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for csgames2016_web project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csgames2016_web.settings")
+
+application = get_wsgi_application()
