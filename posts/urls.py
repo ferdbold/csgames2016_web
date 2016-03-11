4f1528a7346d382922c2f65764167eb8007e496c
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-03-10 22:52:24 -0500

    Included bootstrap and template extension

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'posts'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<post_id>[0-9]+)/$', views.single, name='single')
